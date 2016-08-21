648116f28fefdafd96091e91a40fd8d9797bde77
knightliao <knightliao@gmail.com>
2016-08-21 12:45:22 +0800

    re

--- mybottle/core/app_common.py
+++ mybottle/core/app_common.py
@@ -4,5 +4,5 @@ from bottle import TEMPLATE_PATH, Bottle
 from mybottle.libs import log
 
 app = Bottle()
-TEMPLATE_PATH.append('static/tpl/')
+TEMPLATE_PATH.append('tpl/')
 app.install(log.stopwatch)
