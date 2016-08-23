39ac5eafc9d097e92b3ee3aa10bea2e3c1d591c4
knightliao <knightliao@gmail.com>
2016-08-23 15:21:59 +0800

    re

--- mybottle/core/plugin.py
+++ mybottle/core/plugin.py
@@ -1,4 +1,9 @@
 # -*- coding:utf-8 -*-
+import logging
+from datetime import datetime
+from functools import wraps
+
+from bottle import request, response
 
 from mybottle.core.app_common import app
 
@@ -14,3 +19,24 @@ def add_db_close_plugin(db_instance_list):
         return wrapper
 
     app.install(db_close)
+
+
+def log_to_logger(fn):
+    """
+    Wrap a Bottle request so that a log line is emitted after it's handled.
+    (This decorator can be extended to take the desired logger as a param.)
+    """
+
+    @wraps(fn)
+    def _log_to_logger(*args, **kwargs):
+        request_time = datetime.now()
+        actual_response = fn(*args, **kwargs)
+        # modify this to log exactly what you need:
+        logging.info('%s %s %s %s %s' % (request.remote_addr,
+                                         request_time,
+                                         request.method,
+                                         request.url,
+                                         response.status))
+        return actual_response
+
+    return _log_to_logger
